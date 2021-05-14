05167434770540c9a8742b662b98b0674f7aa518
GEEPA88 <geepaabhijith@gmail.com>
2021-05-14 12:02:00 +0530

    using const widget and constructor

--- lib/main.dart
+++ lib/main.dart
@@ -118,6 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    print('build() MyHonePageState');
     final mediaQuery = MediaQuery.of(context);
     final isLandscape = mediaQuery.orientation == Orientation.landscape;
     final PreferredSizeWidget appbar = Platform.isIOS
