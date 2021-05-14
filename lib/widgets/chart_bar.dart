eeb115bef519cdfa4e7ec1730983ace171726ec9
GEEPA88 <geepaabhijith@gmail.com>
2021-05-14 12:31:21 +0530

    extracting widget

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -5,7 +5,7 @@ class ChartBar extends StatelessWidget {
   final double spendingAmount;
   final double spendingtotalpercent;
 
-  ChartBar(this.label, this.spendingAmount, this.spendingtotalpercent);
+  const ChartBar(this.label, this.spendingAmount, this.spendingtotalpercent);
 
   @override
   Widget build(BuildContext context) {
